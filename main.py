1508f325395cbbbcf9424959903ec5ffe5eab701
Richard-Weiss <76665424+Richard-Weiss@users.noreply.github.com>
2023-10-14 16:58:15 +0200

    feat: use asynchronous functions and add logging

--- main.py
+++ main.py
@@ -1,56 +1,107 @@
+import logging
+import sys
+
+import aiofiles
+import asyncio
+from datetime import date
+import time
 import os
-import requests
+import platform
 import zipfile
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
+import aiohttp
+import structlog
+
+from arsenic import get_session
+from arsenic.browsers import Firefox
+from arsenic.constants import SelectorType
+from arsenic.services import Geckodriver
 
 
-def get_image_tuples(img_url_list: list):
-    options = Options()
-    options.add_argument("-headless")
-    service = Service(gecko_path)
-    driver = webdriver.Firefox(options=options, service=service)
-    img_tuples = []
-    for url in img_url_list:
-        driver.get(url)
-        img = WebDriverWait(driver, 10).until(
-            ec.presence_of_element_located((By.XPATH, "//div[@class='imgContainer']/img"))
-        )
-        img_tuples.append(
-            (img.get_attribute("src"), img.get_attribute("alt"))
-        )
-    driver.quit()
-    os.remove("geckodriver.log")
-    return img_tuples
+async def get_image_tuples(img_url_list: list):
+    results = await asyncio.gather(*map(get_image_from_url, img_url_list))
+    return list(results)
 
 
-if __name__ == "__main__":
-    gecko_path = "C:\\Users\\icepe\\Developer_Tools\\Gecko Driver"
+async def get_image_from_url(url):
+    service = Geckodriver(
+        binary='C:\\Users\\icepe\\Developer_Tools\\Gecko Driver\\geckodriver.exe',
+        log_file=os.devnull
+    )
+    options = Firefox(**{
+        "moz:firefoxOptions": {
+            "args": ["-headless"]
+        }
+    })
+    async with get_session(service, options) as session:
+        await session.get(url)
+        logging.info(f"Getting image for URL: {url}")
+        img = await session.wait_for_element(20, "//div[@class='imgContainer']/img", SelectorType.xpath)
+        return await img.get_attribute("src"), await img.get_attribute("alt")
+
+
+async def download_and_zip_images(image_tuples: list):
+    zip_file = zipfile.ZipFile(f"bing_images_{date.today()}.zip", "w")
+    async with aiohttp.ClientSession() as session:
+        tasks = [
+            download_and_save_image(session, src, alt, index)
+            for index, (src, alt)
+            in enumerate(image_tuples)
+        ]
+        file_names = await asyncio.gather(*tasks)
+        for file_name in file_names:
+            if file_name is not None:
+                file_name: str
+                zip_file.write(file_name)
+                os.remove(file_name)
+    zip_file.close()
+
+
+async def download_and_save_image(session, src, alt, index):
+    try:
+        async with session.get(src) as response:
+            if response.status == 200:
+                filename = f"{alt}_{str(index)}.jpg"
+                async with aiofiles.open(filename, "wb") as f:
+                    await f.write(await response.read())
+                logging.info(f"Downloading image from: {src}")
+                return filename
+            else:
+                print(f"Failed to download {src}")
+    except Exception as e:
+        print(e)
+
+
+def set_arsenic_log_level(level=logging.WARNING):
+    logger = logging.getLogger('arsenic')
+
+    def logger_factory():
+        return logger
+
+    structlog.configure(logger_factory=logger_factory)
+    logger.setLevel(level)
+
+
+async def main():
+    start = time.time()
 
     with open("images_clipboard.txt", "r") as f:
         content = f.read().splitlines()
-    lines = [line for line in content if line != "www.bing.com" and line != ""]
+    lines = [line for line in content
+             if line != "www.bing.com" and line != ""]
     image_url_list = [lines[i + 1] for i in range(0, len(lines), 2)]
-    image_tuples = get_image_tuples(image_url_list)
-    zip_file = zipfile.ZipFile("bing_images.zip", "w")
+    logging.info(f"Preparing {len(image_url_list)} URLs for download...")
+    image_tuples = await get_image_tuples(image_url_list)
+    await download_and_zip_images(image_tuples)
 
-    for index, (src, alt) in enumerate(image_tuples):
-        try:
-            response = requests.get(src)
-            if response.status_code == 200:
-                filename = f"{alt}_{str(index)}.jpg"
-                with open(filename, "wb") as f:
-                    f.write(response.content)
-                zip_file.write(filename)
-                os.remove(filename)
-            else:
-                print(f"Failed to download {src}")
-        except Exception as e:
-            print(e)
+    end = time.time()
+    elapsed = end - start
+    logging.info(f"Finished downloading {len(image_url_list)} images in {round(elapsed, 2)} seconds.")
 
-    zip_file.close()
+
+if __name__ == "__main__":
+    if platform.system() == "Windows":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    set_arsenic_log_level()
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
